ec8a5ad2bcba229b4b9c67225eea45fb786d1a00
Ian Homer <ihomer@worth.systems>
2021-09-08 09:18:49 +0100

    Tweak trying

--- features/steps/tutorial.py
+++ features/steps/tutorial.py
@@ -1,16 +1,16 @@
-from behave import given, when, then
+from behave import *
 
 
 @given("we have behave installed")
-def step_impl(context):
+def when_we_have_installed(_context):
     pass
 
 
 @when("we implement a test")
-def step_impl(context):
+def we_implement_a_test(_context):
     assert True is not False
 
 
 @then("behave will test it for us!")
-def step_impl(context):
+def behave_will_test_it(context):
     assert context.failed is False
